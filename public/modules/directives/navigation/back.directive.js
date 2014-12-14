08f7a81c989ff6546c85d7681637ac249a6f3eca
Andrew Grauch <agrauch@MrBigglesworth.local>
2014-12-14 15:38:56 -0700

    add cancel and footer

---
+++ public/modules/directives/navigation/back.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('directives').directive('back', ['$window', function($window) {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            elem.bind('click', function () {
+                $window.history.back();
+            });
+        }
+    };
+}]);
